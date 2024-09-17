65e725a59a0d50fec018698340928e7a3e85b2d5
egjallo <ejallow@insistglobal.com>
2024-09-17 09:45:46 +0000

    worind auth with cleark authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  let herf = userId ? "/journal" : "/new-user";
   return (
-    
-        <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-          <div className="w-full max-w-[600px] mx-auto">
-            <h1 className="text-6xl mb-4">The Journal app</h1>
-            <p className="text-2xl text-white/60 mb-4">is a digital platform designed to help users document their thoughts, experiences, and daily reflections in a seamless and organized way. </p>
-            <div>
-              <Link href={'/journal'}>
-              <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">get started</button>
-              </Link>
-            </div>
-          </div>
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The Journal app</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          is a digital platform designed to help users document their thoughts,
+          experiences, and daily reflections in a seamless and organized way.{" "}
+        </p>
+        <div>
+          <Link href={herf}>
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
         </div>
+      </div>
+    </div>
   );
 }
